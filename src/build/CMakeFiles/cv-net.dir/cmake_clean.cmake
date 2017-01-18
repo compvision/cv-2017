84a4f9e0daa05fd88e300b98fd54ffc5133de79b
Justin <davisandroid242@gmail.com>
2017-01-18 00:00:20 +0000

    stuffs

---
+++ src/build/CMakeFiles/cv-net.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv-net.dir/cv_b.o"
+  "CMakeFiles/cv-net.dir/NetworkController.o"
+  "cv-net.pdb"
+  "cv-net"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv-net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
