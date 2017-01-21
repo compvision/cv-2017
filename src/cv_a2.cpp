438104d9e3291a66d596b46769d9720e2638ff93
Andy Alawani <anialawani@gmail.com>
2017-01-21 14:29:29 -0800

    Processor works

--- src/cv_a2.cpp
+++ src/cv_a2.cpp
@@ -2,12 +2,13 @@
 #include<iostream>
 
 #include "TargetProcessor.hpp"
+#include "VideoDevice.hpp"
 
 using namespace std;
 
-int Altitude, Azimuth, Distance;
+double Altitude, Azimuth, Distance;
 
-cv::Mat processor2(cv::Mat image, int lower, int upper){
+cv::Mat processor2(cv::Mat image, int minHue, int maxHue, int minVal, int maxVal){
 
     /*
     This function uses a given image, converts the given image into HSV,
@@ -22,11 +23,20 @@ cv::Mat processor2(cv::Mat image, int lower, int upper){
     cv::split(imageHSV, HSVChannels);
 
     cv::Mat imageHue = HSVChannels.at(0).clone();
+    cv::Mat imageValue = HSVChannels.at(2).clone();
 
-    cv::Mat imageLower, imageUpper, imageResult;
-    cv::threshold(imageHue, imageLower, lower, 255, CV_THRESH_BINARY);
-    cv::threshold(imageHue, imageUpper, upper, 255, CV_THRESH_BINARY_INV);
-    imageResult = imageLower & imageUpper;
+    cv::Mat threshLow, threshHigh, hueResult, valResult, imageResult;
+
+    threshold(imageHue, threshLow, minHue, 255, THRESH_BINARY);
+    threshold(imageHue, threshHigh, maxHue, 255, THRESH_BINARY_INV);
+    hueResult = threshLow & threshHigh;
+
+    //Value
+    threshold(imageValue, threshLow, minVal, 255, THRESH_BINARY);
+    threshold(imageValue, threshHigh, maxVal, 255, THRESH_BINARY_INV);
+    valResult = threshLow & threshHigh;
+
+    imageResult = hueResult & valResult;
 
     return imageResult;
 }
@@ -35,29 +45,31 @@ cv::Mat processor2(cv::Mat image, int lower, int upper){
 int main(){
     TargetProcessor processor;
 
-    VideoCapture cap(0);
-
-    if(!cap.isOpened())
-        return -1;
+    VideoDevice* cap = new VideoDevice();
+    cap -> startCapture(1);
 
     for(;;){
         cv::Mat rawImg;
-        cap >> rawImg;
+        rawImg = cap -> getImage();
 
-        cv::Mat processedImg = processor2(rawImg, 25, 50);
+        cv::Mat processedImg = processor2(rawImg, 0, 102, 227, 255);
+
+        cv::imshow("Thingamabob", processedImg);
+
         cv::Canny(processedImg, processedImg, 20, 80);
 
         vector<vector<cv::Point> > contours;
         cv::findContours(processedImg, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
 
-        cv::drawContours(rawImg, contours, -1, cv::Scalar(130, 250, 255));
-
-        for(int j = 0; j < contours.size(); j++){
+        int j, i;
+        for(j = 0; j < contours.size(); j++)
+        {
             vector <cv::Point> output;
             cv::approxPolyDP(contours[j], output, (cv::arcLength(cv::Mat(contours.at(j)), true) * 0.02), true);
-            if(output.size() == 4 && cv::contourArea(output) > 100){
+            if(output.size() == 4 && cv::contourArea(output) > 150){
+                cout << "Found Target\n" << endl;
                 int maxX = 0, minX = 10000, maxY = 0, minY = 10000;
-                for(int i = 0; i < contours.size(); i++){
+                for(i = 0; i < contours.size(); i++){
                     if(output[i].x > maxX){
                         maxX = output[i].x;
                     }
@@ -80,14 +92,21 @@ int main(){
                 Altitude = processor.calculateAltitude();
             }
         }
+        /*cout << "Distance: " <<Distance << endl;
+        cout << "Azimuth: " << Azimuth << endl;
+        cout << "Altitude: " << Altitude << endl;
+        */
+        string disText = "Distance: " + to_string(Distance);
+        string aziText = "Azimuth: " + to_string(Azimuth);
+        string altText = "Altitude: " + to_string(Altitude);
 
-        ;
-        cv::putText(rawImg, "Distance: " + Distance, cv::Point(10, 10), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(255), 3, 8);
-        cv::putText(rawImg, "Azimuth: " + Azimuth, cv::Point(10, 30), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(255), 3, 8);
-        cv::putText(rawImg, "Altitude: " + Altitude, cv::Point(10, 50), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(255), 3, 8);
+        cv::putText(rawImg, disText, cv::Point(10, 50), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(0));
+        cv::putText(rawImg, aziText, cv::Point(10, 100), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(0));
+        cv::putText(rawImg, altText, cv::Point(10, 150), FONT_HERSHEY_SCRIPT_SIMPLEX, 2, Scalar::all(0));
         cv::imshow("Data", rawImg);
-        if(waitKey(30) >= 0)
+        if(cv::waitKey(30) >= 0)
             break;
+
     }
     return 0;
 }
